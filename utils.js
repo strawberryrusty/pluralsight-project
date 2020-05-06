0499f3cb83f9ce3addea4cdd311bad6073f0fad5
Prash Mohan <prashmohanuon@gmail.com>
2020-05-06 22:19:45 +0100

    css

--- utils.js
+++ utils.js
@@ -2,7 +2,11 @@ function showMessage(message) {
   document.getElementById('message').textContent = message
 }
 
-showMessage('title')
+let name = 'prash'
+let message = `Hello
+
+
+${name}`
 
 //single comment
 
@@ -13,8 +17,5 @@ comment */
 // let taxRate = 0.8
 // let price = 1.1 + 1.3
 
-const amount = 0
-
-
-
-showMessage(--amount)
+showMessage(message)
+console.log(message)
