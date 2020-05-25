03ccca4bec4cebe8ca219c73154111f58ee30a1e
Prash Mohan <prashmohanuon@gmail.com>
2020-05-25 22:11:54 +0100

    JS

--- utils.js
+++ utils.js
@@ -183,5 +183,17 @@ header.style.fontWeight = '100'
 const button = document.getElementById('see-review')
 
 button.addEventListener('click', function() {
-  console.log('click')
+
+  const review = document.getElementById('review')
+
+  if (review.classList.contains('d-none')) {
+    review.classList.remove('d-none')
+    button.textContent = 'CLOSE REVIEW'
+  } else {
+    review.classList.add('d-none')
+    button.textContent = 'SEE REVIEW'
+  }
+
+  // review.classList.toggle('d-none')
+  //^ this also  works as a toggle
 })
